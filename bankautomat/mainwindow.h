a1d318f59752a88d1c80bac6103172bff72ab0d3
Teemu Lahtinen <tlahtine@gmail.com>
2021-12-09 17:01:27 +0200

    UI:n hienosäätöä

--- bankautomat/mainwindow.h
+++ bankautomat/mainwindow.h
@@ -17,6 +17,23 @@
 
 #include "customer.h"
 
+#define START_PHASE 0
+#define LOGIN_PHASE 1
+#define CREDIT_PHASE 2
+#define MAIN_PHASE 3
+#define WITHDRAW_PHASE 4
+#define SUM_PHASE 5
+#define BALANCE_PHASE 6
+#define TRANSACTION_PHASE 7
+#define ALERT_PHASE 8
+
+#define DEBIT_WITHDRAWAL 0
+#define CREDIT_WITHDRAWAL 1
+#define DEBIT_PURCHASE 2
+#define CREDIT_PURCHASE 3
+#define DEBIT_DEPOSIT 4
+#define CREDIT_DEPOSIT 5
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -31,44 +48,37 @@ public:
 
 private slots:
     void on_number_clicked();
-
+    void on_pushBack_clicked();
+    void on_pushEnter_clicked();
     void on_pushNosto_clicked();
-
     void on_pushMuu_clicked();
-
     void on_pushSaldo_clicked();
-
     void on_push20_clicked();
-
     void on_pushLopeta_clicked();
-
     void on_push50_clicked();
-
     void on_push100_clicked();
-
     void on_push200_clicked();
-
     void on_timer_timeout();
-
     void on_pushTapahtumat_clicked();
-
-    void on_finish(QNetworkReply *reply);
+    void on_login(QNetworkReply *reply);
+    void on_withdrawal(QNetworkReply *reply);
+    void on_balance_query(QNetworkReply *reply);
+    void on_transactions_query(QNetworkReply *reply);
 
 private:
     Ui::MainWindow *ui;
-    uint8_t vaihe;
+    uint8_t phase;
     Customer *customer;
-    QString alert;
     QTimer *timer = NULL;
     void numpad_event_create();
     void set_main_ui();
     void set_start_ui();
     void set_credit_or_debit_ui();
-    void set_balance_ui(QString text);
+    void set_balance_ui();
     void set_withdraw_ui();
-    void set_transactions_ui(QString text);
+    void set_transactions_ui();
     void set_sum_ui();
-    void set_alert_ui(int alert_time);
+    void set_alert_ui(QString alert_text, int alert_time);
     void delay(int millisecondsToWait);
     void set_timer(int timeout);
     void login(QString korttiID, QString pin);
